01dffa26dd4369f76be41399038905f64db3fcda
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-09-16 15:39:09 +0200

    start2

---
+++ Uebungen/020_kleinste_von_drei_zahlen.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Kleinste von drei Zahlen
+ * 
+ * Schreibe ein Programm, 
+ * das drei Variablen mit zufälligen Zahlen befüllt.
+ * Dann soll das Programm testen,
+ * welcher Zahlenwert der kleinste ist und diesen ausgeben.
+ *
+ * Hilfsmittel: rand(), if-elseif-else
+ */
