874ecf3b2eb556263b5c1bfd69072d509cb59cb4
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-09-22 14:29:48 +0200

    120

---
+++ Uebungen/120_einmaliges_im_array.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Einmaliges im Array
+ *
+ * Schreibe ein Programm, das ein Array mit neun Zahlen befüllt.
+ * Dabei sollen vier Zahlen doppelt vorkommen
+ * und eine Zahl nur einmal.
+ *
+ * Schreibe dann ein Programm, das aus diesem Array die Zahl findet,
+ * die nur einmal vorkommt.
+ */
+
