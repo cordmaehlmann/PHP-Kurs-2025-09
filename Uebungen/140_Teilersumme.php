b61bfdd74f20aef4728950d67a3f33c393e075a4
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-09-23 15:42:02 +0200

    yip

--- Uebungen/140_Teilersumme.php
+++ Uebungen/140_Teilersumme.php
@@ -7,11 +7,11 @@
  * die Summe aller Teiler kleiner als die Zahl ist.
  * Die Zahl selber soll hierbei nicht zu den Teilern zählen.
  *
- * Bei 81 würde TRUE zurück gegeben werden, da
+ * Bei 81 würde TRUE zurückgegeben werden, da
  * 1 + 3 + 9 + 27 = 40
  * und 40 < 81
  *
- * Bei 80 würde FALSE zurück gegeben werden, da
+ * Bei 80 würde FALSE zurückgegeben werden, da
  * 1 + 2 + 4 + 5 + 8 + 10 + 16 + 20 + 40 = 106
  * und 106 > 80
  */
