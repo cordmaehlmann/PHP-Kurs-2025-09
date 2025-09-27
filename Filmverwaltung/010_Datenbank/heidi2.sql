503d70149351dbd15969105eb9b982c35f585116
cordmaehlmann <cordmaehlmann@users.noreply.github.com>
2025-09-27 08:39:49 +0200

    Update heidi2.sql

--- Filmverwaltung/010_Datenbank/heidi2.sql
+++ Filmverwaltung/010_Datenbank/heidi2.sql
@@ -8,7 +8,11 @@ USE filmverwaltung;
 -- INSERT INTO filme (titel, jahr, genre, vertrieb, fsk, einspielergebnis, laenge) VALUES ('The Accountant', 2016, 'Action', 'Warner Bros.', 16, 155.6, 128); 
 
 
-DELETE FROM filme WHERE id=1;
+-- DELETE FROM filme WHERE id=1;
+
+
+UPDATE filme SET titel='The Godfather 3', jahr=1972, genre='Crime/Drama', vertrieb='Paramount Pictures', fsk=16, einspielergebnis=250.34, laenge=175 WHERE id=9;
+
 
 SELECT * FROM filme;
 
